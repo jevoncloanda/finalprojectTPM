7faa31c543f91bfa8eb00380dc4561929ef4634d
RyanFebrian2202 <ryan22febrian@gmail.com>
2022-01-10 01:47:15 +0700

    Memperlengkap controller registrasi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
@@ -76,12 +77,76 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
+
+    //CREATE DATA
+
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'group_name' => $data['group_name'],
             'password' => Hash::make($data['password']),
+            'status' => $data['status'],
+            'leader_name' => $data['leader_name'],
+            'leader_email' => $data['leade_email'],
+            'leader_wa_number' => $data['leader_wa_number'],
+            'leader_line_id' => $data['leader_line_id'],
+            'leader_github' => $data['leader_github'],
+            'leader_birth_place' => $data['leader_birth_place'],
+            'leader_birth_date' => $data['leader_birth_date'],
+            'leader_CV' => $data['leader_CV'],
+            'leader_card' => $data['leader_card'],
         ]);
+    }
+
+
+    //SEARCH DAN VIEW DATA
+
+    public function getLeaderData(Request $request){
+
+        if($request->input('search')){
+            $users = User::where('group_name','like','%'.request('search').'%')->get();
+        } else{
+            $users = User::all();
+        }
+
+        //cara lain pake $appliers = DB::table('appliers')->get();
+        return view('view',['users'=>$users]);
+    }
+
+
+    //UPDATE
+
+    public function getLeaderDataById($id){
+        $user = User::find($id);
+        return view('update',['user' => $user]);
+    }
+
+    public function updateLeaderData(array $data, $id){
+        $user = User::find($id);
+
+        $user->update([
+            'group_name'=> $data['group_name'],
+            'password' => Hash::make($data['password']),
+            'status' => $data['status'],
+            'leader_name' => $data['leader_name'],
+            'leader_email' => $data['leade_email'],
+            'leader_wa_number' => $data['leader_wa_number'],
+            'leader_line_id' => $data['leader_line_id'],
+            'leader_github' => $data['leader_github'],
+            'leader_birth_place' => $data['leader_birth_place'],
+            'leader_birth_date' => $data['leader_birth_date'],
+            'leader_CV' => $data['leader_CV'],
+            'leader_card' => $data['leader_card'],
+        ]);
+
+        return redirect(route('silahkandiisi'));
+    }
+
+
+    //DELETE TEAM
+
+    public function deleteTeam($id){
+        User::destroy($id);
+        return redirect(route('bebas'));
     }
 }
